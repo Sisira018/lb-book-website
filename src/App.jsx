d3249ed2eb2f15bcfc9187076e52efebc742ab65
tzzkenz <127670903+tzzkenz@users.noreply.github.com>
2025-08-15 22:09:41 +0530

    changes discarded

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import Random from "./Random.jsx";
 export default function App() {
   return (
     <>
-      <h1 className="w-2 p-4 mt-4">HI</h1>
-      <button className="bg-blue-500 text-white p-2 rounded">hello</button>
+      <h1>HI</h1>
+      <button>hello</button>
       <Random />
     </>
   );
