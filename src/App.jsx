d5150ab95cc853cf7ac7a0accccbb0931d5db82d
tzzkenz <127670903+tzzkenz@users.noreply.github.com>
2025-08-15 22:08:27 +0530

    Test fo r tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Random from './Random.jsx'
+import Random from "./Random.jsx";
 export default function App() {
-  return(
+  return (
     <>
-    <h1>HI</h1>
-    <button>hello</button>
-    <Random/>
+      <h1 className="w-2 p-4 mt-4">HI</h1>
+      <button className="bg-blue-500 text-white p-2 rounded">hello</button>
+      <Random />
     </>
-  )
+  );
 }
